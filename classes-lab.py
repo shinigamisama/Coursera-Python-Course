48cd4a7ebd7c8008e2c9ac14210e5045b6b13dca
shinigamisama <shinigami.sama.mm@gmail.com>
2023-01-03 17:22:23 +0100

    added classes lab

---
+++ classes-lab.py
@@ -0,0 +1,67 @@
+class analysedText(object):
+    
+    def __init__ (self, text):
+        # remove punctuation
+        formattedText = text.replace('.','').replace('!','').replace('?','').replace(',','')
+        
+        # make text lowercase
+        formattedText = formattedText.lower()
+        
+        self.fmtText = formattedText
+        
+    def freqAll(self):        
+        # split text into words
+        wordList = self.fmtText.split(' ')
+        
+        # Create dictionary
+        freqMap = {}
+        for word in set(wordList): # use set to remove duplicates in list
+            freqMap[word] = wordList.count(word)
+        
+        return freqMap
+    
+    def freqOf(self,word):
+        # get frequency map
+        freqDict = self.freqAll()
+        
+        if word in freqDict:
+            return freqDict[word]
+        else:
+            return 0
+
+import sys
+
+sampleMap = {'eirmod': 1,'sed': 1, 'amet': 2, 'diam': 5, 'consetetur': 1, 'labore': 1, 'tempor': 1, 'dolor': 1, 'magna': 2, 'et': 3, 'nonumy': 1, 'ipsum': 1, 'lorem': 2}
+
+def testMsg(passed):
+    if passed:
+       return 'Test Passed'
+    else :
+       return 'Test Failed'
+
+print("Constructor: ")
+try:
+    samplePassage = analysedText("Lorem ipsum dolor! diam amet, consetetur Lorem magna. sed diam nonumy eirmod tempor. diam et labore? et diam magna. et diam amet.")
+    print(testMsg(samplePassage.fmtText == "lorem ipsum dolor diam amet consetetur lorem magna sed diam nonumy eirmod tempor diam et labore et diam magna et diam amet"))
+except:
+    print("Error detected. Recheck your function " )
+print("freqAll: ")
+try:
+    wordMap = samplePassage.freqAll()
+    print(testMsg(wordMap==sampleMap))
+except:
+    print("Error detected. Recheck your function " )
+print("freqOf: ")
+try:
+    passed = True
+    for word in sampleMap:
+        if samplePassage.freqOf(word) != sampleMap[word]:
+            passed = False
+            break
+    print(testMsg(passed))
+    
+except:
+    print("Error detected. Recheck your function  " )
+
+
+
